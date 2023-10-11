0ee87e6bef65f1c56c74d87d7eeadcf7238cd23b
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-11 22:25:10 +0700

    chat delete

--- src/structures/Chat.js
+++ src/structures/Chat.js
@@ -130,7 +130,7 @@ class Chat extends Base {
   async delete() {
     return this.client.mPage.evaluate((chatId) => {
       return window.WWebJS.sendDeleteChat(chatId);
-    }, this.id._serialized);
+    }, { chatId: this.id._serialized });
   }
 
   /**
